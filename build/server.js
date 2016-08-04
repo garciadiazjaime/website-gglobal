375e38beef0efad150d3d74459221e98013195bb
jaime <garciadiazjaime@gmail.com>
2016-08-04 13:46:02 -0500

    init

---
+++ build/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var bodyParser = require('body-parser');
+var appDir = path.dirname(__dirname);
+
+app.use(express.static(appDir + '/static'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.sendFile(appDir + '/views/home.html');
+});
+
+app.get('/nuestra-filosofia', function (req, res) {
+  res.sendFile(appDir + '/views/aboutus.html');
+});
+
+app.listen(3030);
+console.log("Running at Port 3030");
