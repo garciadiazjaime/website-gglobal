4bd54dfdaee7e40c805e685e09f08d6ee085d9c1
jaime <garciadiazjaime@gmail.com>
2016-08-04 14:51:46 -0500

    adjust server

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ var bodyParser = require('body-parser');
 var appDir = path.dirname(__dirname);
 
 app.use(express.static(appDir + '/static'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
@@ -16,14 +18,16 @@ app.get('/nuestra-filosofia', function(req, res) {
   res.sendFile(appDir + '/views/aboutus.html');
 });
 
-app.set('ipaddress', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3030);
-
-var server = app.listen(app.get('port'), app.get('ipaddress'), (err) => {
-  if (err) {
-    console.log(err);
-  }
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+// error handling
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
 });
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3030;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app;
